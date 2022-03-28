f7aea5cc4023fbc8afe2c78ce1b7c6d2f5bc62c9
Jessica Turner <jmcclung3509@gmail.com>
2022-03-28 11:39:10 -0700

    Sorted menu items by category under all button

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const menu = [
     id: 1,
     title: "buttermilk pancakes",
     category: "breakfast",
+    sort: "1",
     price: 15.99,
     img: "./images/item-1.jpeg",
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
@@ -13,6 +14,7 @@ const menu = [
     id: 2,
     title: "diner double",
     category: "lunch",
+    sort: "2",
     price: 13.99,
     img: "./images/item-2.jpeg",
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
@@ -21,6 +23,7 @@ const menu = [
     id: 3,
     title: "godzilla milkshake",
     category: "shakes",
+    sort: "3",
     price: 6.99,
     img: "./images/item-3.jpeg",
     desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
@@ -29,6 +32,7 @@ const menu = [
     id: 4,
     title: "country delight",
     category: "breakfast",
+    sort: "1",
     price: 20.99,
     img: "./images/item-4.jpeg",
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
@@ -37,6 +41,7 @@ const menu = [
     id: 5,
     title: "egg attack",
     category: "lunch",
+    sort: "2",
     price: 22.99,
     img: "./images/item-5.jpeg",
     desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
@@ -45,6 +50,7 @@ const menu = [
     id: 6,
     title: "oreo dream",
     category: "shakes",
+    sort: "3",
     price: 18.99,
     img: "./images/item-6.jpeg",
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
@@ -53,6 +59,7 @@ const menu = [
     id: 7,
     title: "bacon overflow",
     category: "breakfast",
+    sort: "1",
     price: 8.99,
     img: "./images/item-7.jpeg",
     desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
@@ -61,6 +68,7 @@ const menu = [
     id: 8,
     title: "american classic",
     category: "lunch",
+    sort: "2",
     price: 12.99,
     img: "./images/item-8.jpeg",
     desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
@@ -69,6 +77,7 @@ const menu = [
     id: 9,
     title: "quarantine buddy",
     category: "shakes",
+    sort: "3",
     price: 16.99,
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
@@ -77,6 +86,7 @@ const menu = [
     id: 10,
     title: "spaghetti and meatballs",
     category: "dinner",
+    sort: "4",
     price: 20.99,
     img: "./images/Spaghetti-and-meatballs.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
@@ -85,7 +95,7 @@ const menu = [
     id: 11,
     title: "chicken parmesan",
     category: "dinner",
-    price: 26.99,
+    sort: "4",
     img: "./images/chicken-parmesan.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
@@ -167,7 +177,7 @@ dinnerBtn.addEventListener("click", function() {
 
 
 allBtn.addEventListener("click", function() {
-
+  menu.sort((a,b ) => (a.sort > b.sort) ? 1: -1)
   showMenuItems(menu)
   
 })
